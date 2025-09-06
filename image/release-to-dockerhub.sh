dc356db8bf032a989508d9860aceffb72f10559b
Carlos <me@sudocarlos.com>
2025-09-06 00:19:26 -0400

    tailscale-socaddy-proxy version v1.86.2

--- image/release-to-dockerhub.sh
+++ image/release-to-dockerhub.sh
@@ -8,7 +8,7 @@ IMAGE=tailscale-socaddy-proxy
 PLATFORM=linux/amd64,linux/386
 # bump version
 #docker run --rm -v "$PWD":/app treeder/bump patch
-version=0.1
+version=`awk -F "=" '/TAILSCALE_VERSION=/{print $NF}' Dockerfile`
 echo "Building version: $version"
 # run build
 docker buildx build -t $USERNAME/$IMAGE:latest -t $USERNAME/$IMAGE:$version --push .
